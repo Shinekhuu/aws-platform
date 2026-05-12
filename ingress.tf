1444512153eff67807feccb584aa9d20b0a7f5d5
Shinekhuu <m_shinehuu@yahoo.com>
2026-05-12 17:05:13 +0800

    fixinig Error: Reference to undeclared resource

--- ingress.tf
+++ ingress.tf
@@ -40,3 +40,47 @@ resource "kubernetes_ingress_v1" "argocd" {
     }
   }
 }
+
+resource "kubernetes_ingress_v1" "grafana" {
+
+  depends_on = [
+    helm_release.alb_controller,
+    helm_release.prometheus
+  ]
+
+  metadata {
+    name      = "grafana"
+    namespace = "monitoring"
+
+    annotations = {
+      "kubernetes.io/ingress.class" = "alb"
+
+      "alb.ingress.kubernetes.io/scheme" = "internet-facing"
+
+      "alb.ingress.kubernetes.io/target-type" = "ip"
+    }
+  }
+
+  spec {
+    rule {
+      host = "grafana.${var.domain_name}"
+
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "prometheus-grafana"
+
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
