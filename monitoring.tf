50e3d7f026cd7ae367448d958e2ad6d2cb83b273
Shinekhuu <m_shinehuu@yahoo.com>
2026-05-13 19:21:48 +0800

    is default added

--- monitoring.tf
+++ monitoring.tf
@@ -22,51 +22,29 @@ resource "helm_release" "prometheus" {
   timeout = 1200
 
   values = [
-    <<EOF
-    grafana:
-      adminUser: "${var.grafana_admin_user}"
-      adminPassword: "${var.grafana_admin_password}"
-      additionalDataSources:
-        - name: Loki
-          type: loki
-          access: proxy
-          url: http://loki:3100
+<<EOF
+grafana:
+  adminUser: "${var.grafana_admin_user}"
+  adminPassword: "${var.grafana_admin_password}"
 
-    alertmanager:
-      enabled: false
+  additionalDataSources:
+    - name: Loki
+      type: loki
+      access: proxy
+      url: http://loki:3100
+      isDefault: false
 
-    kubeEtcd:
-      enabled: false
+alertmanager:
+  enabled: false
 
-    kubeScheduler:
-      enabled: false
+kubeEtcd:
+  enabled: false
 
-    kubeControllerManager:
-      enabled: false
+kubeScheduler:
+  enabled: false
+
+kubeControllerManager:
+  enabled: false
 EOF
   ]
-}
-
-resource "helm_release" "loki" {
-
-  depends_on = [
-    helm_release.prometheus
-  ]
-
-  name      = "loki"
-  namespace = "monitoring"
-  create_namespace = true
-
-  repository = "https://grafana.github.io/helm-charts"
-
-  chart = "loki-stack"
-
-  cleanup_on_fail = true
-  wait            = true
-
-  replace         = true
-  force_update    = true
-  recreate_pods   = true
-
-  timeout = 1200
 }
