219a65adc6b7f51d90beebee3ec19d9d38c39473
Shinekhuu <m_shinehuu@yahoo.com>
2026-05-18 07:54:59 +0000

    Argocd image updater

--- provider.tf
+++ provider.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
   }
 }
 
